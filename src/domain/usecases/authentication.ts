79b54ef057ea2ae6501d2754945b55fdc8568bb0
Kátia Cibele Ribeiro da Cunha <katia.cunha@insidesistemas.com.br>
2020-11-03 15:20:18 -0300

    feat: add authentication usecase

---
+++ src/domain/usecases/authentication.ts
@@ -0,0 +1,14 @@
+import {AccountModel} from '../models/AccountModel';
+
+type AuthenticationParams = {
+  email: string
+  password: string
+}
+
+/**PAra implementar a interface Authtentication precisa enviar os parametros de entrada
+ * do tipo AuthtenticationParams e ele retorna uma resposta assincrona, ou seja, um promise.
+ */
+export interface Authentication {
+  
+   auth (params: AuthenticationParams) : Promise<AccountModel>
+}
