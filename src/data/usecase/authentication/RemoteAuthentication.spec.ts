d2472bbc0660ef506a863c577468f3dbe7f997f6
Kátia Cibele Ribeiro da Cunha <katia.cunha@insidesistemas.com.br>
2020-11-03 17:08:00 -0300

    refactor: move SUT cration to a function (design pattern factory)

--- src/data/usecase/authentication/RemoteAuthentication.spec.ts
+++ src/data/usecase/authentication/RemoteAuthentication.spec.ts
@@ -1,15 +1,28 @@
-import { RemoteAuthentication} from './remote-authentication';
-import {HttpPostClientSpy} from '../../test/mock-http-client';
+import { RemoteAuthentication } from './remote-authentication';
+import { HttpPostClientSpy } from '../../test/mock-http-client';
 
+type SutTypes = {
+  sut: RemoteAuthentication
+  httpPostClientSpy: HttpPostClientSpy
+}
+
+//design pattern factory
+const makeSut = (url: string = 'any_url'): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy()
+  const sut = new RemoteAuthentication(url, httpPostClientSpy)
+  return {
+    sut, 
+    httpPostClientSpy
+  }
+
+}
 
 
 describe('RemoteAuthentication', () => {
-  test('Should call Http Client with correct URL', async () => {
-    
-    const url = 'any_url'
-    const httpPostClientSpy = new HttpPostClientSpy()
-    const sut = new RemoteAuthentication(url, httpPostClientSpy)
-    sut.auth();
+  test('Should call Http Client with correct URL', async () => { 
+    const url = 'other_url'
+    const { sut, httpPostClientSpy } = makeSut(url)
+    await sut.auth()
     expect(httpPostClientSpy.url).toBe(url)
   })
 })
