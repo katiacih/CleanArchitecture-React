33d52bf394df53ab80cbbd339849d2bc51184076
Kátia Cibele Ribeiro da Cunha <katia.cunha@insidesistemas.com.br>
2020-11-04 10:06:49 -0300

    refactor:change imports to use modules

--- src/data/usecase/authentication/RemoteAuthentication.spec.ts
+++ src/data/usecase/authentication/RemoteAuthentication.spec.ts
@@ -1,6 +1,6 @@
 import { RemoteAuthentication } from './remote-authentication'
-import { HttpPostClientSpy } from '../../test/mock-http-client'
-import { mockAuthentication} from '../../../domain/tests/mock-authentication';
+import { HttpPostClientSpy } from '@/data/test/mock-http-client'
+import { mockAuthentication} from '@/domain/tests/mock-authentication';
 import faker from 'faker'
 
 type SutTypes = {
