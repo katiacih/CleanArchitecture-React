dd8463cf2390817d5fec0936d6932331d1bcf0ef
Kátia Cibele Ribeiro da Cunha <katia.cunha@insidesistemas.com.br>
2020-11-09 14:17:44 -0300

    test: ensure RequiredFieldValidation returns erro if field is empty

--- src/validation/required-field/required-field-validation.ts
+++ src/validation/required-field/required-field-validation.ts
@@ -1,5 +1,5 @@
 import { FieldValidation } from '@/validation/protocols/field-validation'
-import { RequiredFieldError  } from '@/validation/errors'
+import { RequiredFieldError  } from '@/validation/errors/required-field-error'
 
 export class RequiredFieldValidation implements FieldValidation {
   constructor (readonly field: string) {}
