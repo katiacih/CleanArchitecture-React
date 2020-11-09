70ba76b881988c4902d8f9d8e28ae86f87a7c687
Kátia Cibele Ribeiro da Cunha <katia.cunha@insidesistemas.com.br>
2020-11-09 15:04:39 -0300

    refactor: move required field validation to a different folder

---
+++ src/validation/validators/required-field/required-field-validation.spec.ts
@@ -0,0 +1,27 @@
+import { RequiredFieldValidation } from './required-field-validation'
+import { RequiredFieldError  } from '@/validation/errors/required-field-error'
+import faker from 'faker'
+
+const makeSut = (): RequiredFieldValidation => new RequiredFieldValidation(faker.database.column())
+
+describe('RequiredFieldValidation', () => {
+
+  test('Should return error if field email is empty', () => {
+    const sut = makeSut()
+    const error = sut.validate('')
+    expect(error).toEqual(new RequiredFieldError())
+  })
+
+  test('Should return error if field password is empty', () => {
+    const sut = makeSut()
+    const error = sut.validate('')
+    expect(error).toEqual(new RequiredFieldError())
+  })
+
+  test('Should return falsy if field is not empty', () => {
+    const sut = makeSut()
+    const error = sut.validate(faker.random.word())
+    expect(error).toBeFalsy()
+  })
+
+})
