b4485a5f834f1ac9b7eaffdec8e59aadc936a32f
Kátia Cibele Ribeiro da Cunha <katia.cunha@insidesistemas.com.br>
2020-11-09 14:28:45 -0300

    feat: ensure RequiredFieldValidation returns falsy if field is not empty

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -2,9 +2,17 @@ import { RequiredFieldValidation } from './required-field-validation'
 import { RequiredFieldError  } from '@/validation/errors/required-field-error'
 
 describe('RequiredFieldValidation', () => {
-  test('Should return error if field is empty', () => {
+
+  test('Should return error if field email is empty', () => {
     const sut = new RequiredFieldValidation('email')
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFieldError())
   })
+
+  test('Should return error if field password is empty', () => {
+    const sut = new RequiredFieldValidation('password')
+    const error = sut.validate('')
+    expect(error).toEqual(new RequiredFieldError())
+  })
+  
 })
