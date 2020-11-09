51ae903acef246b366a5ff5a16f2d49274b02b01
Kátia Cibele Ribeiro da Cunha <katia.cunha@insidesistemas.com.br>
2020-11-09 10:33:39 -0300

    feat: prevent Login to call submite more than once

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -32,6 +32,9 @@ const Login: React.FC<Props> = ({validation, authentication}: Props) => {
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault()
+    if(state.isLoading){
+      return
+    }
     setState({ ...state, isLoading: true })
     await authentication.auth({
       email: state.email,
