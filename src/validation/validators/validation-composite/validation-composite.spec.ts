6ffc1d1255a5ea86f304a7050d01e72fa8a438d9
Kátia Cibele Ribeiro da Cunha <katia.cunha@insidesistemas.com.br>
2020-11-09 16:21:58 -0300

    test: ensure ValidationComposite returns the first error found

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -4,6 +4,7 @@ import { ValidationComposite } from './validation-composite'
 describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
     const fieldValidationSpy = new FieldValidationSpy('any_field')
+    fieldValidationSpy.error = new Error('first_error_message')
     const fieldValidationSpy2 = new FieldValidationSpy('any_field')
     fieldValidationSpy2.error = new Error('second_error_message')
     const sut = new ValidationComposite([
