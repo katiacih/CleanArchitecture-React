d91ed18f9f5bcaf080e71a1c36485aa14b34f057
Kátia Cibele Ribeiro da Cunha <katia.cunha@insidesistemas.com.br>
2020-11-09 13:37:27 -0300

    test: ensure Login navigate to main page on success

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -18,7 +18,7 @@ type SutParams = {
   validationError:string
 }
 
-const history = createMemoryHistory()
+const history = createMemoryHistory( {initialEntries: ['/login']})
 
 const makeSut = (params?: SutParams): SutTypes => {
   const validationStub = new ValidationStub()
@@ -163,6 +163,8 @@ describe('Login Component', () => {
     simulateValidSubmit(sut)
     await waitFor(() =>  sut.getByTestId('form'))
     expect(localStorage.setItem).toHaveBeenCalledWith('accessToken', authenticationSpy.account.accessToken)
+    expect(history.length).toBe(1)
+    expect(history.location.pathname).toBe('/')
 
   })
 
