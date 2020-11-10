be66198c49b30fa8bdef85d82787cd3b9a50bb29
Kátia Cibele Ribeiro da Cunha <katia.cunha@insidesistemas.com.br>
2020-11-10 13:58:22 -0300

    chore: add files to coverage ignore

--- src/infra/http/axios-htttp-client/axios-http-client.spec.ts
+++ src/infra/http/axios-htttp-client/axios-http-client.spec.ts
@@ -1,12 +1,10 @@
 import { AxiosHttpClient} from './axios.http.client'
-import { mockAxios, mockHttpResponse } from '@/infra/test/index'
+import { mockAxios, mockHttpResponse } from '@/infra/test/'
 import { mockPostRequest } from '@/data/test/index'
 import axios from 'axios'
 
 
-
 jest.mock('axios')
-
 
 type SubTypes = {
   sut: AxiosHttpClient,
@@ -27,7 +25,7 @@ const makeSut = (): SubTypes => {
 describe('AxiosHttpClient', () => {
 
   test('Should call axios with correct values', async () => {
-    const request = mockPostRequest()
+    const request = mockPostRequest ()
     const {sut, mockedAxios } = makeSut()
     await sut.post(request)
     expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
--- src/infra/http/axios-htttp-client/axios.http.client.ts
+++ src/infra/http/axios-htttp-client/axios.http.client.ts
@@ -2,15 +2,18 @@ import { HttpPostClient, HttpPostParams, HttpResponse } from '@/data/protocols/h
 import axios, { AxiosResponse } from 'axios';
 
 export class AxiosHttpClient implements HttpPostClient<any, any> {
- async post(params: HttpPostParams<any>) : Promise<HttpResponse<any>>
+ async post(params: HttpPostParams<any>): Promise<HttpResponse<any>>
  {
   let httpResponse: AxiosResponse<any>
-    try {
-      httpResponse = await axios.post(params.url, params.body)
-    } catch (error) {
-      httpResponse = error.response
-    }
-
+  try {
+    httpResponse = await axios.post(params.url, params.body)
+  } catch (error) {
+    httpResponse = error.response
+  }
+  return {
+    statusCode: httpResponse.status,
+    body: httpResponse.data
+  }
  }
 
 }
