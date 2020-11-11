fc533e018fe17d09ae70608afb8429ff7d4a79c3
Kátia Cibele Ribeiro da Cunha <katia.cunha@insidesistemas.com.br>
2020-11-11 14:09:26 -0300

    test: ensure RemoAddAccount calls HttpPostClient with correct body

--- src/data/usecase/add-account/remote-add-account.spec.ts
+++ src/data/usecase/add-account/remote-add-account.spec.ts
@@ -20,7 +20,6 @@ const makeSut = (url: string = faker.internet.url()): SutTypes => {
 }
 
 describe('RemoteAddAccount', () => {
-  
   test('Should call HttpPostClient with correct URL', async () => {
     const url = faker.internet.url()
     const { sut, httpPostClientSpy } = makeSut(url)
