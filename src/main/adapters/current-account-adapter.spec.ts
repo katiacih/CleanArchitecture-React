718c7d69ed740ca403b2781696457f2607f9a0df
Kátia Cibele Ribeiro da Cunha <katia.cunha@insidesistemas.com.br>
2020-11-16 13:45:02 -0300

    test: ensure CurrentAccountAdapter throws if an invalid account is correct and provider

---
+++ src/main/adapters/current-account-adapter.spec.ts
@@ -0,0 +1,21 @@
+import { setCurrentAccountAdapter } from './current-account-adapter'
+import { mockAccountModel } from '@/domain/test'
+import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter'
+import { UnexpectedError } from '@/domain/errors'
+
+jest.mock('@/infra/cache/local-storage-adapter')
+
+describe('CurrentAccountAdapter', () => {
+  test('Should call LocalStorageAdapter with correct values', () => {
+    const account = mockAccountModel()
+    const setSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set')
+    setCurrentAccountAdapter(account)
+    expect(setSpy).toHaveBeenCalledWith('account', account)
+  })
+
+  test('Should throw UnexpectedError', () => {
+    expect(() => {
+      setCurrentAccountAdapter(undefined)
+    }).toThrow(new UnexpectedError())
+  })
+})
