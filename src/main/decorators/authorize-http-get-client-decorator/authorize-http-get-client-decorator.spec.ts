931ba8a57149438474a78e86b67442ed2ef16cdd
Kátia Cibele Ribeiro da Cunha <katia.cunha@insidesistemas.com.br>
2020-11-17 16:36:20 -0300

    feat: ensure AuthorizeHttpGetClientDecorator not add token ig GetStorage is invalid

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
@@ -1,11 +1,42 @@
 import { AuthorizeHttpGetClientDecorator } from '@/main/decorators'
-import { mockGetRequest, GetStorageSpy } from '@/data/test'
+import { mockGetRequest, GetStorageSpy, HttpGetClientSpy } from '@/data/test'
+import faker from 'faker'
+import { HttpGetParams } from '@/data/protocols/http'
+
+type SutTypes = {
+  sut: AuthorizeHttpGetClientDecorator
+  getStorageSpy: GetStorageSpy
+  httpGetClientSpy: HttpGetClientSpy
+}
+
+const makeSut = (): SutTypes => {
+  const getStorageSpy = new GetStorageSpy()
+  const httpGetClientSpy = new HttpGetClientSpy()
+  const sut = new AuthorizeHttpGetClientDecorator(getStorageSpy, httpGetClientSpy)
+  return {
+    sut,
+    getStorageSpy,
+    httpGetClientSpy
+  }
+}
 
 describe('AuthorizeHttpGetClientDecorator', () => {
-  test('Should call GetStorage with correct value', () => {
-    const getStorageSpy = new GetStorageSpy()
-    const sut = new AuthorizeHttpGetClientDecorator(getStorageSpy)
-    sut.get(mockGetRequest())
+  test('Should call GetStorage with correct value', async () => {
+    const { sut, getStorageSpy } = makeSut()
+    await sut.get(mockGetRequest())
     expect(getStorageSpy.key).toBe('account')
+  })
+
+  test('Should not add headers if GetStorage is invalid', async () => {
+    const { sut, httpGetClientSpy } = makeSut()
+    const httpRequest: HttpGetParams = {
+      url: faker.internet.url(),
+      headers: {
+        field: faker.random.words()
+      }
+    }
+    await sut.get(httpRequest)
+    expect(httpGetClientSpy.url).toBe(httpRequest.url)
+    expect(httpGetClientSpy.headers).toEqual(httpRequest.headers)
   })
 })
