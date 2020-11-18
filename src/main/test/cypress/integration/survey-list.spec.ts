c485cb3813560d4815c05b5f7d2607502376acba
Kátia Cibele Ribeiro da Cunha <katia.cunha@insidesistemas.com.br>
2020-11-18 11:48:23 -0300

    test: ensure SurveyList presents error UnexpectedError and AccessDeniedError

---
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -0,0 +1,22 @@
+import * as Helper from '../support/helpers'
+import * as Http from '../support/survey-list-mocks'
+import faker from 'faker'
+
+describe('SurveyList', () => {
+  beforeEach(() => {
+    Helper.setLocalStorageItem('account', { accessToken: faker.random.uuid(), name: faker.name.findName() })
+  })
+
+  it('Should present error on UnexpectedError', () => {
+    Http.mockUnexpectedError()
+    cy.visit('')
+    cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
+  })
+
+  it('Should present correct username', () => {
+    Http.mockUnexpectedError()
+    cy.visit('')
+    const { name } = Helper.getLocalStorageItem('account')
+    cy.getByTestId('username').should('contain.text', name)
+  })
+})
